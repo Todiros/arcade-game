2810359b814c3245a52448bde2f62226b4290729
Tony Borisov <tony.d.borisov@gmail.com>
2018-05-26 22:48:53 +0300

    feat: randomized enemy movement

--- js/app.js
+++ js/app.js
@@ -1,19 +1,36 @@
-// Enemies our player must avoid
 class Enemy {
     constructor() {
-        this.rows = [60, 140, 220, 300];
-        const randomRow = Math.round(Math.random() * 3);
+        // random number between 0 and 3, coresponding to each row
+        let randomRow = Math.round(Math.random() * 3);
+        this.rows = [60, 145, 230, 315];
 
-        this.x = 5;
+        // initial position, outside canvas between -100 and -200
+        this.x = -(Math.round(Math.random() * 100) + 100);
         this.y = this.rows[randomRow];
 
+        // random speed setter (80 - 300)
+        this.speed = Math.round(Math.random() * 220) + 80;
         this.sprite = 'images/enemy-bug.png';
     }
 
+    setInitialX() {
+        return -(Math.round(Math.random() * 100) + 100);
+    }
+
+    setRow() {
+        return Math.round(Math.random() * 3);
+    }
+
     update(dt) {
-        // You should multiply any movement by the dt parameter
-        // which will ensure the game runs at the same speed for
-        // all computers.
+        this.x += dt * this.speed;
+
+        if (this.x > 500) {
+            // reset the enemy to random position behind the canvas
+            this.x = this.setInitialX();
+
+            // randomizing enemy position on each update
+            this.y = this.rows[this.setRow()];
+        }
     }
 
     render() {
@@ -24,27 +41,35 @@ class Enemy {
 class Player {
     constructor() {
         this.x = 200;
-        this.y = 405;
-        this.sprite = 'images/char-boy.png';
+        this.y = 400;
+        
         this.lives = 3;
         this.score = 0;
+        this.sprite = 'images/char-boy.png';
+    }
+
+    // player's starting position
+    reset() {
+        this.x = 200; 
+        this.y = 400;
     }
 
     handleInput(key) {
         if (key === 'left' || key === 'a') {
             if (this.x > 0)
-                this.x -= 100;
+                this.x -= 100; // one column to the left
         } else if (key === 'up' || key === 'w') {
-            if (this.y > -20) 
-                this.y -= 85;
+            if (this.y > 0) 
+                this.y -= 85; // one row up
             else
-                this.x = 200, this.y = 405;
+                // reset if player gets to the top aka wins
+                this.reset();
         } else if (key === 'right' || key === 'd') {
             if (this.x < 400)
-                this.x += 100; 
+                this.x += 100; // one column to the right
         } else if (key === 'down' || key === 's') {
-            if (this.y < 405)
-                this.y += 85;
+            if (this.y < 400)
+                this.y += 85; // one row down
         }
     }
 
@@ -61,7 +86,8 @@ let allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy()];
 let player = new Player();
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Player.handleInput() method. You don't need to modify this
+// and yet, I did
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
